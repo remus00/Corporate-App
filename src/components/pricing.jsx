1ad6d9dc138f6c8405f76f4c326a2f13befc210e
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-10-03 11:48:39 +0200

    Pricing Component

---
+++ src/components/pricing.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+
+const pricingData = [
+    {
+        id: 1,
+        plan: "Basic",
+        price: "£49",
+        features: [
+            "Wireframing",
+            "Visual Design",
+            "5 pages",
+            "Free Hosting",
+            "Support & Assistance",
+        ],
+        link: "https://www.google.com",
+    },
+    {
+        id: 2,
+        plan: "Premium",
+        price: "£149",
+        features: [
+            "Wireframing",
+            "Visual Design",
+            "15 pages",
+            "Free Hosting",
+            "Support & Assistance",
+        ],
+        link: "https://www.facebook.com",
+    },
+    {
+        id: 3,
+        plan: "Ultimate",
+        price: "£349",
+        features: [
+            "Wireframing",
+            "Visual Design",
+            "Unlimited pages",
+            "Free Hosting",
+            "Support & Assistance",
+        ],
+        link: "https://www.twitter.com",
+    },
+];
+
+const AppPricing = () => {
+    return (
+        <section id="pricing" className="block pricing-block">
+            <Container fluid>
+                <div className="title-holder">
+                    <h2>Pricing &amp; plans</h2>
+                    <div className="subtitle">
+                        check our pricing &amp; plans
+                    </div>
+                </div>
+                <Row>
+                    {pricingData.map((pricing) => (
+                        <Col sm={4} key={pricing.id}>
+                            <div className="heading">
+                                <h3>{pricing.plan}</h3>
+                                <span className="price">{pricing.price}</span>
+                            </div>
+                            <div className="content">
+                                <ListGroup>
+                                    {pricing.features.map((feature, index) => (
+                                        <ListGroup.Item key={index}>
+                                            {feature}
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            </div>
+                            <div className="btn-holder">
+                                <a
+                                    href={pricing.link}
+                                    className="btn btn-primary"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    Order Now
+                                </a>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </section>
+    );
+};
+
+export default AppPricing;
